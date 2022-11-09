56b9a46258114f94b38db34dd92bbdf58fcc00bc
Christian Rojas <rojaschristian96@gmail.com>
2022-11-09 12:06:26 -0600

    added resume and about section

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
-import Typewriter from 'typewriter-effect';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -16,23 +15,28 @@ export default function Home() {
       <main>
         <section className="bg-gray-800 min-h-screen"> 
         <nav className="py-10 mb-12 flex justify-between pr-3">
-          <h1 className="text-xl text-black"></h1>
+          <h1 className="text-xl"></h1>
           <ul className="flex items-right">
-             <li className="px-2">About</li>
-             <li className="px-2">Experience</li>
+             <Link href="/#aboutme"><li className="px-2">About</li></Link>
+             <Link href="#experience"><li className="px-2">Experience</li></Link>
              <li className="px-2">Projects</li>
              <li className="px-2">Contact</li>
             <li>
-              <a className="bg-green-400 text-xl px-4 py-2 rounded-md ml-8" href='#'>Resume</a>
+              <script>
+                function download() {
+                  const URL
+                }
+              </script>
+              <a className="bg-green-400 text-xl px-4 py-2 rounded-md ml-8" href="/public/ChristianRojasResume_22.pdf" target="_blank" rel="noopener noreferrer"><button>Resume</button></a>
             </li>
           </ul>
         </nav>
         <div className="text-gray-300"> 
         </div>
-        <div className="text-gray-300 pl-6 pr-6">
+        <div className="text-gray-300 pl-6 pr-10 font-sans">
           <h4 className=" text-cyan-500 text-left">Hi, my name is</h4>
-          <h1 className="text-4xl py-2 font-medium">Christian Rojas</h1>
-          <p className="text-md py-5 leading-8">
+          <h1 className="text-6xl py-2 font-bold font-sans">Christian Rojas.</h1>
+          <p className="text-md py-5 leading-8 font-sans pr-10">
             I'm a software developer with a passion for building applications that 
             are accessible to all users. My experience ranges from web development, 
             to mobile and web application development. Currently, 
@@ -45,14 +49,69 @@ export default function Home() {
         </div>
         </section>
 
-        <section className="text-black text-center p-10 bg-gray-800 min-h-screen">
-          <h3 className="text-3xl py-1">About Me</h3>
-          <p className="text-md py-3 leading-8">Over the years, I have cultivated my 
-          development skills in these languages:</p>
-          <div>
-            <div>
+        <section id="aboutme" className="text-gray-300 text-left bg-gray-800 min-h-screen pl-6 pr-6 font-sans">
+          <div className="pr-10 font-sans">
+            <h3 className="text-3xl py-1 font-bold">About Me</h3>
+            <div className="pr-10 font-sans">
+            <p className="text-md py-3 leading-8">Hi! my name is Christian. I have mostly grown up on the internet, having learned my first language (HTML) at 17 years old. The interest in software started when I was
+            building small websites in my school computer lab because I loved the idea of being able to build anything I could think up.
+            </p>
+            <p className="text-md py-3 leading-8">Since then, I have built websites for myself, friends, and clients. Now, I am working with a <a className="text-cyan-500" href="https://www.logicalposition.com/">marketing agency</a> to provide web 
+              solutions to clients. I focus on building accessible websites, while helping clients maintain 
+              good rankings.
+              </p>
+            <p className="text-md py-3 leading-8">I am also a big lover of building web and mobile applications, most notably 
+                using JavaScript, Node, Next, and React. I am a big fan of Python and C++ too, 
+                using them to build robust applications.</p>
+            <p className="text-md py-3 leading-8">Here are a few of the technologies I have been working on.</p>
+
+            <div class="table pl-6">
+            <div class="table-header-group">
+              <div class="table-cell ">
+              <li className="list-disc">JavaScript</li>
+              </div>
+              <div class="table-cell pl-12">
+              <li className="list-disc">Node.js</li>
+              </div>
+            </div>
+            <div class="table-column-group">
+              <div class="table-colum">One Column</div>
+              <div class="table-colum">Two Column</div>
+            </div>
+            <div class="table-row-group pl-5">
+              <div class="table-row">
+                <div class="table-cell">
+                  <li className="list-disc">React</li></div>
+                <div class="table-cell pl-12">
+                <li className="list-disc">Next.js</li>
+                </div>
+              </div>
+              <div class="table-row">
+                <div class="table-cell">
+                  <li className="list-disc">WordPress</li>
+                </div>
+                <div class="table-cell pl-12">
+                <li className="list-disc">Tailwind</li>
+                </div>
+              </div>
+              <div class="table-row">
+                <div class="table-cell">
+                  <li className="list-disc">Python</li>
+                </div>
+                <div class="table-cell pl-12">
+                <li className="list-disc">SQL</li>
+                </div>
+              </div>
             </div>
           </div>
+
+
+            </div>
+          </div>
+        </section>
+
+        <section id="experience">
+
         </section>
       </main>
     </div>
